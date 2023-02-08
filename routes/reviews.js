a3d239f10212b9c9ef677c87fb9bf8a44a80fe44
marta szuran <marta.szuran@dataart.com>
2023-02-08 13:24:39 +0100

    first db connection

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const { MongoClient } = require('mongodb');
+const uri = process.env.DATABASE_URI;
 
 router.get('/', (req, res) => {
-  res.send('succes');
+  dbAccess();
+  res.send('hi');
 })
+
+async function dbAccess() {
+  const client = new MongoClient(uri);
+  try {
+    await client.connect();
+    console.log('connected to db');
+    const dbLists = await client.db().admin().listDatabases();
+    console.log(dbLists.databases);
+    return dbLists.databases;
+  } catch (e) {
+    console.error(e);
+  }
+}
 
 module.exports = router;
